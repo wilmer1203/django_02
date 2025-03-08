068e1f30cc47c6e1a7f268890320781d79baa325
Wilmer <wilmeraguirre2002.3@gmail.com>
2025-03-08 11:31:27 -0400

    page about

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 # Todas las urls de la aplicación website
 
 from django.urls import path
-from website.views import HomePageView
+from website.views import HomePageView, AboutPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
